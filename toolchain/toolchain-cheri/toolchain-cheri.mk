e1d06ca726f44c54e3d91421b676fbd26ce5373c
Lukas Auer <lukas.auer@aisec.fraunhofer.de>
2021-03-18 12:23:26 +0100

    toolchain-cheri: adjust name of ld.so to match name defined in glibc-cheri
    Signed-off-by: Lukas Auer <lukas.auer@aisec.fraunhofer.de>

--- toolchain/toolchain-cheri/toolchain-cheri.mk
+++ toolchain/toolchain-cheri/toolchain-cheri.mk
@@ -9,8 +9,10 @@ CHERI_LIBC = $(call qstrip,$(BR2_TOOLCHAIN_CHERI_LIBC))
 ifeq ($(BR2_RISCV_64),y)
 ifeq ($(CHERI_LIBC),musl-cheri)
 CHERI_LDSO = /lib/ld-musl-$(TARGET_ARCH_CLANG)-clang.so.1
-else ifeq ($(CHERI_LIBC),glibc-cheri)
-CHERI_LDSO = /lib/ld-linux-$(TARGET_ARCH_CLANG)-l64pc128d.so.1
+else ifeq ($(BR2_RISCV_CHERI)$(CHERI_LIBC),yglibc-cheri)
+CHERI_LDSO = /lib/ld-linux-riscv64-l64pc128d.so.1
+else ifeq ($(BR2_RISCV_CHERI)$(CHERI_LIBC),glibc-cheri)
+CHERI_LDSO = /lib/ld-linux-riscv64-lp64d.so.1
 endif
 endif
 
