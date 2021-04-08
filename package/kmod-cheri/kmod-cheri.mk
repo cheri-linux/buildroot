63d946aa7ab8143f9227d2c5409b986029a65a1f
Julian Horsch <julian.horsch@aisec.fraunhofer.de>
2021-04-08 17:31:23 +0200

    package/kmod-cheri: cherified

--- package/kmod-cheri/kmod-cheri.mk
+++ package/kmod-cheri/kmod-cheri.mk
@@ -4,106 +4,118 @@
 #
 ################################################################################
 
-KMOD_VERSION = 27
-KMOD_SOURCE = kmod-$(KMOD_VERSION).tar.xz
-KMOD_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kmod
-KMOD_INSTALL_STAGING = YES
-KMOD_DEPENDENCIES = host-pkgconf
-HOST_KMOD_DEPENDENCIES = host-pkgconf
+KMOD_CHERI_VERSION = 27
+KMOD_CHERI_SOURCE = kmod-$(KMOD_CHERI_VERSION).tar.xz
+KMOD_CHERI_SITE = $(BR2_KERNEL_MIRROR)/linux/utils/kernel/kmod
+KMOD_CHERI_INSTALL_STAGING = YES
+KMOD_CHERI_DEPENDENCIES = host-pkgconf
+#HOST_KMOD_CHERI_DEPENDENCIES = host-pkgconf
 
 # license info for libkmod only, conditionally add more below
-KMOD_LICENSE = LGPL-2.1+ (library)
-KMOD_LICENSE_FILES = libkmod/COPYING
+KMOD_CHERI_LICENSE = LGPL-2.1+ (library)
+KMOD_CHERI_LICENSE_FILES = libkmod/COPYING
 
 # --gc-sections triggers binutils ld segfault
 # https://sourceware.org/bugzilla/show_bug.cgi?id=21180
 ifeq ($(BR2_microblaze),y)
-KMOD_CONF_ENV += cc_cv_LDFLAGS__Wl___gc_sections=false
+KMOD_CHERI_CONF_ENV += cc_cv_LDFLAGS__Wl___gc_sections=false
 endif
 
 # static linking not supported, see
 # https://git.kernel.org/cgit/utils/kernel/kmod/kmod.git/commit/?id=b7016153ec8
-KMOD_CONF_OPTS = --disable-static --enable-shared
+KMOD_CHERI_CONF_OPTS = --disable-static --enable-shared
 
-KMOD_CONF_OPTS += --disable-manpages
-HOST_KMOD_CONF_OPTS = --disable-manpages
+# CHERI
+KMOD_CHERI_CONF_ENV += \
+	CC="$(TARGET_CC_CLANG)" \
+	LD="$(TARGET_LD_CLANG)"
+
+KMOD_CHERI_CONF_OPTS += \
+	--prefix=/cheri/usr \
+	--exec-prefix=/cheri/usr
+
+
+KMOD_CHERI_CONF_OPTS += --disable-manpages
+#HOST_KMOD_CHERI_CONF_OPTS = --disable-manpages
 
 ifeq ($(BR2_PACKAGE_BASH_COMPLETION),y)
-KMOD_CONF_OPTS += --with-bashcompletiondir=/usr/share/bash-completion/completions
+KMOD_CHERI_CONF_OPTS += --with-bashcompletiondir=/usr/share/bash-completion/completions
 endif
 
-ifeq ($(BR2_PACKAGE_ZLIB),y)
-KMOD_DEPENDENCIES += zlib
-KMOD_CONF_OPTS += --with-zlib
+ifeq ($(BR2_PACKAGE_ZLIB_CHERI),y)
+KMOD_CHERI_DEPENDENCIES += zlib-cheri
+KMOD_CHERI_CONF_OPTS += --with-zlib
 else
-KMOD_CONF_OPTS += --without-zlib
+KMOD_CHERI_CONF_OPTS += --without-zlib
 endif
 
-ifeq ($(BR2_PACKAGE_XZ),y)
-KMOD_DEPENDENCIES += xz
-KMOD_CONF_OPTS += --with-xz
+ifeq ($(BR2_PACKAGE_XZ_CHERI),y)
+KMOD_CHERI_DEPENDENCIES += xz-cheri
+KMOD_CHERI_CONF_OPTS += --with-xz
 else
-KMOD_CONF_OPTS += --without-xz
+KMOD_CHERI_CONF_OPTS += --without-xz
 endif
 
-ifeq ($(BR2_PACKAGE_OPENSSL),y)
-KMOD_DEPENDENCIES += openssl
-KMOD_CONF_OPTS += --with-openssl
+ifeq ($(BR2_PACKAGE_OPENSSL_CHERI),y)
+KMOD_CHERI_DEPENDENCIES += openssl-cheri
+KMOD_CHERI_CONF_OPTS += --with-openssl
 else
-KMOD_CONF_OPTS += --without-openssl
+KMOD_CHERI_CONF_OPTS += --without-openssl
 endif
 
-ifeq ($(BR2_PACKAGE_PYTHON)$(BR2_PACKAGE_PYTHON3),y)
-KMOD_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON),python,python3)
-KMOD_CONF_OPTS += --enable-python
+ifeq ($(BR2_PACKAGE_PYTHON_CHERI)$(BR2_PACKAGE_PYTHON3_CHERI),y)
+KMOD_CHERI_DEPENDENCIES += $(if $(BR2_PACKAGE_PYTHON_CHERI),python-cheri,python3-cheri)
+KMOD_CHERI_CONF_OPTS += --enable-python
 endif
 
-ifeq ($(BR2_PACKAGE_KMOD_TOOLS),y)
+ifeq ($(BR2_PACKAGE_KMOD_CHERI_TOOLS),y)
 
 # add license info for kmod tools
-KMOD_LICENSE += , GPL-2.0+ (tools)
-KMOD_LICENSE_FILES += COPYING
+KMOD_CHERI_LICENSE += , GPL-2.0+ (tools)
+KMOD_CHERI_LICENSE_FILES += COPYING
+
+# CHERI: the /cheri prefix does not support a merged usr setup
 
 # /sbin is really /usr/sbin with merged /usr, so adjust relative symlink
-ifeq ($(BR2_ROOTFS_MERGED_USR),y)
-KMOD_BIN_PATH = ../bin/kmod
-else
-KMOD_BIN_PATH = ../usr/bin/kmod
-endif
+# ifeq ($(BR2_ROOTFS_MERGED_USR),y)
+# KMOD_CHERI_BIN_PATH = ../bin/kmod
+# else
+# KMOD_CHERI_BIN_PATH = ../usr/bin/kmod
+# endif
 
-define KMOD_INSTALL_TOOLS
+define KMOD_CHERI_INSTALL_TOOLS
 	for i in depmod insmod lsmod modinfo modprobe rmmod; do \
-		ln -sf $(KMOD_BIN_PATH) $(TARGET_DIR)/sbin/$$i; \
+		ln -sf ../usr/bin/kmod $(TARGET_DIR)/cheri/sbin/$$i; \
 	done
 endef
 
-KMOD_POST_INSTALL_TARGET_HOOKS += KMOD_INSTALL_TOOLS
+KMOD_CHERI_POST_INSTALL_TARGET_HOOKS += KMOD_CHERI_INSTALL_TOOLS
 else
-KMOD_CONF_OPTS += --disable-tools
+KMOD_CHERI_CONF_OPTS += --disable-tools
 endif
 
-ifeq ($(BR2_PACKAGE_HOST_KMOD_GZ),y)
-HOST_KMOD_DEPENDENCIES += host-zlib
-HOST_KMOD_CONF_OPTS += --with-zlib
-else
-HOST_KMOD_CONF_OPTS += --without-zlib
-endif
-
-ifeq ($(BR2_PACKAGE_HOST_KMOD_XZ),y)
-HOST_KMOD_DEPENDENCIES += host-xz
-HOST_KMOD_CONF_OPTS += --with-xz
-else
-HOST_KMOD_CONF_OPTS += --without-xz
-endif
-
-# We only install depmod, since that's the only tool used for the
-# host.
-define HOST_KMOD_INSTALL_TOOLS
-	mkdir -p $(HOST_DIR)/sbin/
-	ln -sf ../bin/kmod $(HOST_DIR)/sbin/depmod
-endef
-
-HOST_KMOD_POST_INSTALL_HOOKS += HOST_KMOD_INSTALL_TOOLS
+#ifeq ($(BR2_PACKAGE_HOST_KMOD_CHERI_GZ),y)
+#HOST_KMOD_CHERI_DEPENDENCIES += host-zlib
+#HOST_KMOD_CHERI_CONF_OPTS += --with-zlib
+#else
+#HOST_KMOD_CHERI_CONF_OPTS += --without-zlib
+#endif
+#
+#ifeq ($(BR2_PACKAGE_HOST_KMOD_CHERI_XZ),y)
+#HOST_KMOD_CHERI_DEPENDENCIES += host-xz
+#HOST_KMOD_CHERI_CONF_OPTS += --with-xz
+#else
+#HOST_KMOD_CHERI_CONF_OPTS += --without-xz
+#endif
+#
+## We only install depmod, since that's the only tool used for the
+## host.
+#define HOST_KMOD_CHERI_INSTALL_TOOLS
+#	mkdir -p $(HOST_DIR)/sbin/
+#	ln -sf ../bin/kmod $(HOST_DIR)/sbin/depmod
+#endef
+#
+#HOST_KMOD_CHERI_POST_INSTALL_HOOKS += HOST_KMOD_CHERI_INSTALL_TOOLS
 
 $(eval $(autotools-package))
-$(eval $(host-autotools-package))
+#$(eval $(host-autotools-package))
